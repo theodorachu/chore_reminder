df7f5665bb30a6bb6677e626e1f35b92615a01ae
Theodora Chu <theodora@stripe.com>
2019-03-03 13:47:57 -0800

    pulling from column 1 for date of email sending, column 2 for email to send to, column 3 for chore

--- script.py
+++ script.py
@@ -1,21 +1,65 @@
+# using sheets
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
 # using SendGrid's Python Library
 # https://github.com/sendgrid/sendgrid-python
 import sendgrid
 import os
 from sendgrid.helpers.mail import *
 
-# using excel
+#datetime
+import datetime
+now = datetime.datetime.now()
 
+# variables
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+SPREADSHEET_ID = '157HR8D8jJCmZldtQL78wfsM58O_2kIEUusM1KZXm7bw'
+RANGE_NAME = 'Chores!A4:C50'
 
-sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
-from_email = Email("chores@2202bryant.com")
-to_email = Email("dennisaxu@gmail.com")
-subject = "Sending with SendGrid is Fun"
-content = Content("text/plain", "and easy to do anywhere, even with Python")
-mail = Mail(from_email, subject, to_email, content)
-response = sg.client.mail.send.post(request_body=mail.get())
-print(response.status_code)
-print(response.body)
-print(response.headers)
+def main():
+	creds = None
+	if os.path.exists('token.pickle'):
+		with open('token.pickle', 'rb') as token:
+			creds = pickle.load(token)
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+			creds = flow.run_local_server()
+		with open('token.pickle', 'wb') as token:
+			pickle.dump(creds, token)
+	service = build('sheets', 'v4', credentials=creds)
+
+	sheet = service.spreadsheets()
+	result = sheet.values().get(spreadsheetId = SPREADSHEET_ID, range = RANGE_NAME).execute()
+	values = result.get('values', [])
+	
+	if not values:
+		print('No data found.')
+	else:
+		for row in values: #Date, task, person
+			print('%s, %s, %s' % (row[0], row[1], row[2]))
+			date = row[0]
+			today = now.strftime("%d/%m/%Y")
+			if date == today:
+				sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+				from_email = Email("chores@2202bryant.com")
+				to_email = Email(row[2])
+				subject = "It's your chore week!"
+				content = Content("text/plain", "Your chore for the week is %s" %(row[1]))
+				mail = Mail(from_email, subject, to_email, content)
+				response = sg.client.mail.send.post(request_body=mail.get())
+				print(response.status_code)
+				print(response.body)
+				print(response.headers)
+
+if __name__ == '__main__':
+	main()
 
 
